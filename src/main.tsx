a23f769b8ce7bde5e780ae2fe4b7586b4e4b599f
Marcelo Amorim <marcelosmorim.fa@gmail.com>
2026-07-17 13:58:08 +0100

    Global CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { App } from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <App />
+    <h1>Olá, Mundo!</h1>
+    <App />
     <App />
   </StrictMode>,
 )
